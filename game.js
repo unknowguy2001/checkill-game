93bf050c0ed55d80b4c701434868514f4e8d4846
Unknowguy <73591183+unknowguy2001@users.noreply.github.com>
2024-03-29 21:08:34 +0700

    First commit.

---
+++ game.js
@@ -0,0 +1,16 @@
+const board = document.getElementById("board");
+
+// prettier-ignore
+const starter = [
+    "", "X", "", "X", "", "X", "", "X",
+    "X", "", "X", "", "X", "", "X", "",
+    "", "X", "", "X", "", "X", "", "X",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "Y", "", "Y", "", "Y", "", "Y", "",
+    "", "Y", "", "Y", "", "Y", "", "Y",
+    "Y", "", "Y", "", "Y", "", "Y", "",
+];
+
+//!TODO: create renderGame function.
+const renderGame = () => {};
