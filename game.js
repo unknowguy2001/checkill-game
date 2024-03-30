312478076b8d9b96a312256690893253f3f100ee
Unknowguy <73591183+unknowguy2001@users.noreply.github.com>
2024-03-30 15:53:05 +0700

    feat: render board and checker piece

--- game.js
+++ game.js
@@ -12,5 +12,46 @@ const starter = [
     "Y", "", "Y", "", "Y", "", "Y", "",
 ];
 
-//!TODO: create renderGame function.
-const renderGame = () => {};
+const renderGame = () => {
+  let isBlack = true;
+
+  for (let i = 0; i < starter.length; i++) {
+    let square = document.createElement("div");
+
+    square.classList.add("square");
+    square.setAttribute("id", i);
+
+    isBlack
+      ? square.classList.add("black-square")
+      : square.classList.add("white-square");
+
+    board.appendChild(square);
+
+    if (starter[i] != "") {
+      let checkerPiece = document.createElement("div");
+      checkerPiece.classList.add("checker-piece");
+
+      if (starter[i] == "X") {
+        checkerPiece.classList.add("red-checker");
+      } else {
+        checkerPiece.classList.add("blue-checker");
+      }
+
+      square.appendChild(checkerPiece);
+    }
+
+    if (
+      i == 7 ||
+      i == 15 ||
+      i == 23 ||
+      i == 31 ||
+      i == 39 ||
+      i == 47 ||
+      i == 55
+    ) {
+      continue;
+    }
+    isBlack = !isBlack;
+  }
+};
+renderGame();
